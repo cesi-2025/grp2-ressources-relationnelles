9d8c8a4f148ce2d7a5066c6cda3d538848645311
Chamil MUSAYEV <musayevchamil@gmail.com>
2026-03-11 14:06:00 +0100

    feat(interactions): add comment request validation

---
+++ api/app/Http/Requests/CommentRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CommentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, array<int, string>>
+     */
+    public function rules(): array
+    {
+        return [
+            'content' => ['required', 'string', 'min:2', 'max:2000'],
+        ];
+    }
+}
