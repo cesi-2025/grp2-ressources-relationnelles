bff6ca17e44e508315ee608d7a9f453de3ba0e01
Chamil MUSAYEV <musayevchamil@gmail.com>
2026-03-11 13:36:08 +0100

    feat(resources): implement resources and categories endpoints

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ResourceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function () {
@@ -19,3 +21,10 @@ Route::get('/admin/ping', function () {
         'status' => 'admin-ok',
     ]);
 })->middleware(['auth:sanctum', 'role:admin,super_admin']);
+
+Route::get('/resources', [ResourceController::class, 'index']);
+Route::get('/resources/{id}', [ResourceController::class, 'show']);
+Route::post('/resources', [ResourceController::class, 'store'])->middleware(['auth:sanctum']);
+Route::put('/resources/{resource}', [ResourceController::class, 'update'])->middleware(['auth:sanctum']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
