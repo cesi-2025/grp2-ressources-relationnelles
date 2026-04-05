69fc9c8675f9438b341eae5ca4f712fe9618f727
Léon HEU <leon.heu@viacesi.fr>
2026-04-05 13:02:22 +0200

    Testing Layout

--- apps/mobile/app/(authentification)/_layout.tsx
+++ apps/mobile/app/(authentification)/_layout.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { View, ScrollView, StyleSheet, Text } from "react-native";
-import { Link } from "expo-router";
-import { LinearGradient } from "expo-linear-gradient";
-import { BlurView } from "expo-blur";
+import { View, StyleSheet, Text } from "react-native";
 import theme from "@/src/theme";
 
 function AuthLayout() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Home screen</Text>
-            <Link href="/about" style={styles.button}>
-                Go to About screen
-            </Link>
+            <Text style={styles.title}>Ressource Relationnel</Text>
         </View>
     );
 }
@@ -20,16 +14,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: theme.colors.primary,
-        alignItems: "center",
-        justifyContent: "center",
+        paddingTop: 56,
     },
-    text: {
-        color: theme.colors.background,
-    },
-    button: {
-        fontSize: 20,
-        textDecorationLine: "underline",
-        color: theme.colors.accent,
+    title: {
+        paddingLeft: 20,
+        paddingTop: 25,
+        color: "#fff",
+        fontSize: 24,
+        fontWeight: "700",
+        letterSpacing: 0.5,
+        textAlign: "left",
     },
 });
 
