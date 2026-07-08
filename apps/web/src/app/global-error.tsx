8a7ae9868f251b916d4aaf32f4e3ce1dd9238b5b
Chamil MUSAYEV <musayevchamil@gmail.com>
2026-07-08 18:19:58 +0200

    feat: add Sentry error monitoring to API and web app

---
+++ apps/web/src/app/global-error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import * as Sentry from "@sentry/nextjs";
+import { useEffect } from "react";
+
+export default function GlobalError({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <html lang="fr">
+      <body>
+        <h2>Une erreur est survenue</h2>
+      </body>
+    </html>
+  );
+}
